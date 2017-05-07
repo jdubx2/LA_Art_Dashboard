1aeeb2a3fa0bf7eb893e2bd848cb95558ecfd4d6
jdubx2 <jesse.weiss.2017@marhsall.usc.edu>
2017-05-07 12:22:32 -0700

    Initial commit

---
+++ server.R
@@ -0,0 +1,196 @@
+library(shiny)
+
+library(dplyr)
+library(lubridate)
+library(scales)
+library(plotly)
+
+library(rCharts)
+library(rjson)
+library(zipcode)
+
+art <- read.csv("artproject.csv")
+
+art$project.START.date <- mdy(art$project.START.date)
+art$END.DATE <- mdy(art$END.DATE)
+
+art <- mutate(art, 
+              END.DATE = if_else(STATUS == "active",Sys.Date(),END.DATE),
+              proj.duration = END.DATE - project.START.date,
+              Agency = ifelse(AGENCY %in% c("DWP","CDD","Public Works","Zoo","Harbor"),"Other",as.character(AGENCY)),
+              proj.year = year(project.START.date))
+
+data(zipcode)
+
+# scatter axis definition
+xax <- list(
+  autotick = FALSE,
+  ticks = "outside",
+  tick0 = 0,
+  dtick = 50,
+  ticklen = 0,
+  tickwidth = 2,
+  tickcolor = toRGB("black"),
+  title="Duration (Days)",
+  zeroline=F
+)
+yax <- list(title="Full 1% Amount",
+            zeroline=F)
+
+# barplot axis definition
+
+barY <- function(total){
+  ticks <- round(total / 10, -5)
+  return(ticks)
+}
+
+xax2 <- list(
+  autotick = FALSE,
+  ticks = "outside",
+  tick0 = 1991,
+  dtick = 1,
+  ticklen = 5,
+  tickwidth = 2,
+  tickcolor = toRGB("white"),
+  title="",
+  zeroline=F,
+  showline=F
+)
+
+#plot margins
+
+m <- list(
+  l = 50,
+  r = 50,
+  b = 50,
+  t = 50,
+  pad = 2
+)
+
+
+shinyServer(function(input, output) {
+  
+  # scatterplot function
+  
+  output$scatPlot <- renderPlotly({
+    
+    agencyFilter <- if(length(input$agency) == 0){unique(art$Agency)}
+    else{input$agency}
+    
+    councFilter <- if(length(input$councdist) == 0){unique(art$project.CD)}
+    else(input$councdist)
+    
+    dateFilter <- input$dateRange2
+    
+    
+    art <- art %>%
+      filter(!is.na(project.START.date)) %>%
+      filter(Agency %in% agencyFilter & project.CD %in% councFilter) %>%
+      filter(project.START.date >= dateFilter[1] & project.START.date <= dateFilter[2]) %>%
+      mutate(proj.duration = round(proj.duration/7,0))
+    
+    plot_ly(art, type='scatter',
+            x = ~proj.duration, 
+            y = ~FULL.1..amount, 
+            color =~Agency, 
+            colors = "Set1",
+            symbol = ~STATUS, 
+            symbols = c('diamond','circle'),
+            hoverinfo='text',
+            text = ~paste('Project: ', PROJECT.NAME, 
+                          '<br>Agency: ', AGENCY,
+                          '<br>Amount: ', round(FULL.1..amount/1000,0),
+                          'K<br>Start Date: ', project.START.date,
+                          '<br>End Date: ', END.DATE),
+            marker = list(size = 14,
+                          line = list(color = 'black',width = 1),
+                          opacity=.7)) %>%
+      layout(xaxis=xax, yaxis=yax, title="Cost vs Project Duration", margin=m)
+    
+  })
+  
+  # barplot function
+  
+  output$barPlot <- renderPlotly({
+    
+    agencyFilter <- if(length(input$agency) == 0){unique(art$Agency)}
+    else{input$agency}
+    
+    councFilter <- if(length(input$councdist) == 0){unique(art$project.CD)}
+    else(input$councdist)
+    
+    dateFilter <- input$dateRange2
+    
+    art <- art %>%
+      filter(!(is.na(project.START.date))) %>%
+      filter(Agency %in% agencyFilter & project.CD %in% councFilter) %>%
+      filter(project.START.date >= dateFilter[1] & project.START.date <= dateFilter[2]) %>%
+      group_by(proj.year,Agency) %>%
+      summarise(total = sum(FULL.1..amount))
+    
+    yax2 <- list(
+      zeroline=F,
+      tick0 = 0,
+      dtick = barY(max(art$total)),
+      title="Project $"
+    )
+    
+    plot_ly(art, type='bar',
+            orientation = 'v',
+            x = ~proj.year, 
+            y = ~total,
+            color = ~Agency,
+            colors='Set1',
+            hoverinfo='text',
+            text = ~paste('Total : $', round(total / 1000,0), 
+                          'K<br>Agency: ', Agency,sep=""),
+            marker = list(line = list(color='rgba(0, 0, 0, 0)',width=.8)),
+            opacity=.8)%>%
+      layout(barmode = 'stack', yaxis=yax2, xaxis=xax2, title="Expenditures by Year", legend = list(x = 0, y = 1), margin = m)
+  })
+  
+  
+  #base map
+  
+  output$baseMap <- renderMap({
+    
+    basemap <- Leaflet$new()
+    basemap$setView(c(34.038391, -118.326625), 11)
+    basemap$tileLayer(provider = 'OpenStreetMap.DE')
+    
+    basemap$params$width <- 900
+    basemap$params$height <- 550
+    
+    basemap
+    
+  })
+  
+  #data overlay
+  
+  output$dataMap <- renderUI({
+    
+    zip_art <- art %>%
+      filter(project.ZIP != 90050) %>%
+      mutate(region = as.character(project.ZIP)) %>%
+      group_by(region) %>%
+      summarise(value = sum(FULL.1..amount))
+    
+    zip_art$region <- clean.zipcodes(zip_art$region)
+    
+    zip_art <- zip_art %>%
+      left_join(zipcode, by = c("region" = "zip")) %>%
+      select(latitude, longitude, value) %>%
+      mutate(value = log(value)*5) %>%
+      toJSONArray2(json = F, names = F)
+    
+    zip_art <- rjson::toJSON(zip_art)
+    
+    tags$body(tags$script(HTML(sprintf("
+                                       var addressPoints = %s
+                                       var heat = L.heatLayer(addressPoints).addTo(map)           
+                                       ",zip_art))))
+    
+  })
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(shinythemes)
+
+library(dplyr)
+library(lubridate)
+library(scales)
+library(plotly)
+
+library(rCharts)
+library(rjson)
+library(zipcode)
+
+art <- read.csv("artproject.csv")
+
+art$project.START.date <- mdy(art$project.START.date)
+art$END.DATE <- mdy(art$END.DATE)
+
+art <- mutate(art, 
+              END.DATE = if_else(STATUS == "active",Sys.Date(),END.DATE),
+              proj.duration = END.DATE - project.START.date,
+              Agency = ifelse(AGENCY %in% c("DWP","CDD","Public Works","Zoo","Harbor"),"Other",as.character(AGENCY)),
+              proj.year = year(project.START.date))
+
+agency_list <- unique(art$Agency)
+cd_list <- sort(unique(art$project.CD))
+
+
+shinyUI(navbarPage("LA Public Art Dashboard", tabPanel("Home"),
+                   
+                   theme = shinytheme("sandstone"),
+                   
+                   sidebarLayout(
+                     sidebarPanel(width=3,
+                                  selectizeInput(
+                                    'agency', 'Agency (select 1 or more) :', choices = agency_list, multiple = TRUE),
+                                  selectizeInput(
+                                    'councdist', 'Council District (select 1 or more) :', choices = cd_list, multiple = TRUE),
+                                  dateRangeInput('dateRange2',
+                                                 label = 'Project Date Filter',
+                                                 start = mdy("09-16-1991"), end = Sys.Date(),
+                                                 min = mdy("09-16-1991"), max = Sys.Date(),
+                                                 separator = " - ", format = "mm/dd/yy",
+                                                 startview = 'year', language = 'en', weekstart = 1),
+                                  submitButton("Apply Filter")
+                     ),
+                     
+                     mainPanel(
+                       tabsetPanel(type = "tabs", 
+                                   tabPanel("Cost vs Duration", 
+                                            fluidRow(plotlyOutput("scatPlot",height="530px"))),
+                                   tabPanel("Expenditures by Year", 
+                                            fluidRow(plotlyOutput("barPlot", width="95%", height="530px"))),
+                                   tabPanel("Funding Heatmap", 
+                                            fluidRow(chartOutput("baseMap",'leaflet'),
+                                                     tags$head(tags$script(src="http://leaflet.github.io/Leaflet.heat/dist/leaflet-heat.js")),
+                                                     uiOutput('dataMap')))
+                       )
+                     )
+                   )
+))
